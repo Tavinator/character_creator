f67488440bbe0c846d659c9672f9bcb500d5d201
Tav <tscandiffpirvu@urbanbound.com>
2016-01-18 16:46:11 -0600

    its better.. i think

--- ability_modifier_bonus.rb
+++ ability_modifier_bonus.rb
@@ -1,19 +1,22 @@
-# require "./ability_selector"
+require "./ability_selector"
+class AbilityModifierBonus < AbilitySelector
+  def initialize()
+    selector = AbilitySelector.new
+    selection = selector.assign_abilities
+    @scores = selector.the_scores
+  end
+
+  def writeit
+
+    modifiers@scores.each {|k,v| puts "#{k[0..2].upcase}  #{(((v / 2).floor) -5)}"}
+  end
+end
 
 
-ability_info = {"Strength" => 18, "Dexterity" => 15, "Constitution" => 20, "Intelligence" => 8, "Wisdom" => 1, "Charisma" => 10}
-
-ability_info.each {|k,v| puts "#{k[0..2].upcase}  #{(((v / 2).floor) -5)}"}
+# ability_info.each {|k,v| puts "#{k[0..2].upcase}  #{(((v / 2).floor) -5)}"}
 
 
-# class AbilityModifierBonus < AbilitySelector
-#   def initialize()
-#     @ability_bonus = @@ability_scores.clone
-#   end
-# end
+modifier_creator = AbilityModifierBonus.new
+modifier_calculator = modifier_creator.writeit
+puts modifier_calculator
 
-# selector = AbilitySelector.new
-# selection = selector.assign_abilities
-
-# puts "test"
-# puts @ability_bonus
