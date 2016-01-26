0098a2ed8ff514bfff347a7e712cb04d71e597d0
Tav <tscandiffpirvu@urbanbound.com>
2016-01-25 19:55:40 -0600

    refactored and reformated!

--- ability_selector.rb
+++ ability_selector.rb
@@ -10,39 +10,70 @@ class AbilitySelector
   end
 
   def assign_abilities
-    puts "\nYour total score is " + @total.to_s
-    while @ability_scores.count < 5
-      puts "Your scores are: #{@score_totals.inspect}\n"
-      puts "Of the following abilities\n #{@ability_names.inspect}\n"
-      puts "What score do you want #{@ability_names.first} to be set to?\n"
+    input_checker
+    print "...and thus by the process of elimiation\n"
+    line_dashes
+    show_what_was_set
+    @ability_scores[@ability_names.first] = @score_totals.first
+    @ability_names.delete(@ability_names.first)
+    # puts @ability_scores
+  end
 
-      select = gets.chomp.to_i
-      if @score_totals.include?(select)
-        if @score_totals.count(select) > 1
-          multiples_of_the_same_score = @score_totals.count(select)
-          @score_totals.delete(select)
-          @score_totals.fill(select, @score_totals.size,multiples_of_the_same_score-1)
-        else
-          @score_totals.delete(select)
-        end
-        puts "\nYour #{@ability_names.first} has been set to #{select}\n"
-        @ability_scores[@ability_names.first] = select
+  def the_scores
+    @ability_scores
+  end
+
+  def input_checker
+    line_dashes
+    description
+    puts "The total sum of your rolls was: " + @total.to_s + " so we kept it!"
+    while @ability_scores.count < 5
+      print "The scores that you need to set are: #{@score_totals.inspect}\n"
+      print "Of the following abilities: #{@ability_names.inspect}\n"
+      print "What score do you want #{@ability_names.first} to be set to?  "
+      user_input
+      line_dashes
+      if @score_totals.include?(@select)
+        multiple_checker
+        show_what_was_set
+        @ability_scores[@ability_names.first] = @select
         @ability_names.delete(@ability_names.first)
       else
         puts "\nPlease enter a score that is one of the remaining #{@score_totals.count} ability scores\n"
       end
       @score_totals = @score_totals.sort.reverse
-      puts @ability_scores
     end
-    puts "And thus by the process of elimiation"
-    puts "Your #{@ability_names.first} has been set to #{select}\n"
-    @ability_scores[@ability_names.first] = @score_totals.first
-    @ability_names.delete(@ability_names.first)
-    puts @ability_scores
   end
 
-  def the_scores
-    @ability_scores
+  def user_input
+    @select = gets.chomp.to_i
+  end
+
+  def multiple_checker
+    if @score_totals.count(@select) > 1
+      multiples_of_the_same_score = @score_totals.count(@select)
+      @score_totals.delete(@select)
+      @score_totals.fill(@select, @score_totals.size,multiples_of_the_same_score-1)
+    else
+      @score_totals.delete(@select)
+    end
+  end
+
+  def line_dashes
+    print "-" * 75
+  end
+
+  def show_what_was_set
+    print "\nYour #{@ability_names.first.upcase} has been set to #{@select}!\n"
+  end
+
+  def description
+    puts "\nTo begin we are rolling 4 D6's and if any of those rolls are a 1, we reroll it"
+    puts "Then we remove the lowest number and the remaining 3 die add up to become one of your scores"
+    puts "This is done 6 times, aka 1 time for each ability"
+    puts "We have let the computer do the rolling for you to save time"
+    puts "Oh, and in the event the total of those rolls is less than 78, we do it all over again!"
+    puts "Alright lets see what you got"
   end
 end
 
